d83df866a193ca0ba47f5f987d8eab72ea2bad51
Chris Sperandio <chris@sperand.io>
2017-08-11 11:06:07 -0700

    fallback to in-memory storage engine upon exceeding localstorage quota (#7)
    * refactor: move away from having operators on individual StoreItems and to move most of that logic to the top level Store. You now access each component of the store via a unified store.set|get|remove(itemKey, ...etc) interface vs as an individual instance variable on the store itself with its own set of methods. This allows us to more ergonomically conduct operations on the entire store in the event of an error with a specific item.
    
    *bugfix*: swap to inMemory storage engine in the case of a localstorage overflow

---
+++ test/engine.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var defaultEngine = require('../lib/engine').defaultEngine;
+var inMemoryEngine = require('../lib/engine').inMemoryEngine;
+var assert = require('proclaim');
+
+// Skip the "supported" tests when its not actually supported
+var xdescribe = window.localStorage ? describe : describe.skip;
+
+describe('localStorage', function() {
+  var engine;
+
+  xdescribe('when supported', function() {
+    beforeEach(function() {
+      engine = defaultEngine;
+      engine.clear();
+    });
+
+    it('should function', function() {
+      engine.setItem('test-key', 'abc');
+      assert.strictEqual(engine.getItem('test-key'), 'abc');
+      assert.strictEqual(engine.length, 1);
+      assert.strictEqual(engine.key(0), 'test-key');
+
+      engine.removeItem('test-key');
+      assert.strictEqual(engine.getItem('test-key'), null);
+      assert.strictEqual(engine.length, 0);
+
+      engine.setItem('test-key', 'abc');
+      engine.clear();
+      assert.strictEqual(engine.length, 0);
+    });
+  });
+
+  describe('when not supported', function() {
+    beforeEach(function() {
+      engine = inMemoryEngine;
+      engine.clear();
+    });
+
+    it('should function', function() {
+      engine.setItem('test-key', 'abc');
+      assert.strictEqual(engine.getItem('test-key'), 'abc');
+      assert.strictEqual(engine.length, 1);
+      assert.strictEqual(engine.key(0), 'test-key');
+
+      engine.removeItem('test-key');
+      assert.strictEqual(engine.getItem('test-key'), null);
+      assert.strictEqual(engine.length, 0);
+
+      engine.setItem('test-key', 'abc');
+      engine.clear();
+      assert.strictEqual(engine.length, 0);
+    });
+  });
+});
--- test/store.test.js
+++ test/store.test.js
@@ -1,45 +1,122 @@
 'use strict';
 
-var StoreItem = require('../lib/store').StoreItem;
-var localStorage = require('../lib/localStorage').localStorage;
+var Store = require('../lib/store');
+var engine = require('../lib/engine').defaultEngine;
+var inMemoryEngine = require('../lib/engine').inMemoryEngine;
 var assert = require('proclaim');
+var each = require('@ndhoule/each');
 
-describe('StoreItem', function() {
-  var item;
+describe('Store', function() {
+  var store;
+  var keys = {
+    IN_PROGRESS: 'inProgress',
+    QUEUE: 'queue',
+    ACK: 'ack',
+    RECLAIM_START: 'reclaimStart',
+    RECLAIM_END: 'reclaimEnd'
+  };
 
   beforeEach(function() {
-    localStorage.clear();
-    item = new StoreItem('key');
+    engine.clear();
+    store = new Store('name', 'id', keys, engine);
   });
 
   describe('.get', function() {
     it('should default to null', function() {
-      assert.strictEqual(item.get(), null);
+      each(function(k) {
+        assert.strictEqual(store.get(k), null);
+      }, keys);
     });
 
     it('should de-serialize json', function() {
-      localStorage.setItem('key', '["a","b",{}]');
-      assert.deepEqual(item.get(), [ 'a', 'b', {} ]);
+      each(function(k) {
+        engine.setItem('name.id.' + k, '["a","b",{}]');
+        assert.deepEqual(store.get(k), [ 'a', 'b', {} ]);
+      }, keys);
     });
 
     it('should return null if value is not valid json', function() {
-      localStorage.setItem('key', '[{]}');
-      assert.strictEqual(item.get(), null);
+      engine.setItem('name.id.queue', '[{]}');
+      assert.strictEqual(store.get(keys.QUEUE), null);
     });
   });
 
   describe('.set', function() {
     it('should serialize json', function() {
-      item.set(['a', 'b', {}]);
-      assert.strictEqual(localStorage.getItem('key'), '["a","b",{}]');
+      each(function(k) {
+        store.set(k, ['a', 'b', {}]);
+        assert.strictEqual(engine.getItem('name.id.' + k), '["a","b",{}]');
+      }, keys);
     });
   });
 
   describe('.remove', function() {
     it('should remove the item', function() {
-      localStorage.setItem('key', '"a"');
-      item.remove();
-      assert.strictEqual(localStorage.getItem('key'), null);
+      each(function(k) {
+        store.set(k, 'a');
+        store.remove(k);
+        assert.strictEqual(engine.getItem('name.id.' + k), null);
+      }, keys);
+    });
+  });
+
+  describe('._createValidKey', function() {
+    it('should return compound if no keys specd', function() {
+      store = new Store('name', 'id');
+      assert.strictEqual(store._createValidKey('test'), 'name.id.test');
+    });
+
+    it('should return undefined if invalid key', function() {
+      store = new Store('name', 'id', { nope: 'wrongKey' });
+      assert.strictEqual(store._createValidKey('test'), undefined);
+    });
+
+    it('should return compound if valid key', function() {
+      assert.strictEqual(store._createValidKey('queue'), 'name.id.queue');
+    });
+  });
+
+  // Skip the "swap" tests when not applicable
+  var xdescribe = window.localStorage ? describe : describe.skip;
+
+  xdescribe('._swapEngine', function() {
+    it('should switch the underlying storage mechanism', function() {
+      assert.strictEqual(store.engine, engine);
+      store._swapEngine();
+      assert.strictEqual(store.engine, inMemoryEngine);
+    });
+
+    it('should swap upon quotaExceeded on set', function() {
+      var lsProxy =  {
+        length: window.localStorage.length,
+        setItem: function(k, v) {
+          return window.localStorage.setItem(k, v);
+        },
+        getItem: function(k) {
+          return window.localStorage.getItem(k);
+        },
+        removeItem: function(k) {
+          return window.localStorage.removeItem(k);
+        },
+        clear: function() {
+          return window.localStorage.clear();
+        },
+        key: function(i) {
+          return window.localStorage.key(i);
+        }
+      };
+
+      store = new Store('name', 'id', keys, lsProxy);
+
+      each(function(v) {
+        store.set(v, 'stuff');
+      }, keys);
+      store.engine.setItem = function() {
+        // eslint-disable-next-line no-throw-literal
+        throw { code: 22, number: -2147024882 };
+      };
+      store.set(keys.QUEUE, 'other');
+      assert.strictEqual(store.get(keys.QUEUE), 'other');
     });
   });
 });
