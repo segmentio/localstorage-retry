6921fb8e187b125b8bd39c10eb1977a58c3f5cc9
Dave <dhaden@atlassian.com>
2021-03-10 16:23:26 +1100

    #27 Prevent reclaim from running if host computer is running slow

---
+++ test/schedule.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var lolex = require('lolex');
+var assert = require('proclaim');
+var sinon = require('sinon');
+var Schedule = require('../lib/schedule');
+
+describe('Schedule', function() {
+  var clock;
+  var schedule;
+
+  beforeEach(function() {
+    clock = lolex.createClock(0);
+    Schedule.setClock(clock);
+    schedule = new Schedule();
+  });
+
+  afterEach(function() {
+    Schedule.resetClock();
+  });
+
+  describe('run', function() {
+    it('should call task after timeout', function() {
+      var timeout = 500;
+      var spyFn = sinon.spy();
+      schedule.run(spyFn, timeout);
+      clock.tick(timeout);
+      assert(spyFn.calledOnce);
+    });
+
+    it('should call task after timeout even after long duration', function() {
+      var timeout = 500;
+      var spyFn = sinon.spy();
+      schedule.run(spyFn, timeout, false, false);
+      clock.setSystemTime(timeout * 2);
+      clock.tick(timeout);
+      assert(spyFn.calledOnce);
+    });
+
+    it('should not call task if past duration factor', function() {
+      var timeout = 500;
+      var spyFn = sinon.spy();
+      schedule.run(spyFn, timeout, true, false);
+      // Fast forwards time but doesnt trigger timers
+      clock.setSystemTime(timeout * 2);
+      // Trigger timers here
+      clock.tick(timeout);
+      assert(spyFn.notCalled);
+      clock.tick(timeout);
+      // Ensure task is not rescheduled
+      assert(spyFn.notCalled);
+    });
+
+    it('should reschedule and call task if skipped', function() {
+      var timeout = 500;
+      var spyFn = sinon.spy();
+      schedule.run(spyFn, timeout, true, true);
+      // Fast forwards time but doesnt trigger timers
+      clock.setSystemTime(timeout * 2);
+      // Trigger timers here
+      clock.tick(timeout);
+      assert(spyFn.notCalled);
+      clock.tick(timeout);
+      assert(spyFn.calledOnce);
+    });
+  });
+});
